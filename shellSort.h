d59912a2b424db13aae282fcf55cb6bc34e35e7e
zfoteff <zfoteff@zagmail.gonzaga.edu>
2019-12-09 17:43:35 -0800

    a

---
+++ shellSort.h
@@ -0,0 +1,20 @@
+#ifndef SHELLSORT_H
+#define SHELLSORT_H
+
+class ShellSort
+{
+ public:
+ 
+ ShellSort();
+ 
+ void printOriginalArray(int arr[], int len);
+ 
+ void shellSort(int arr[], int len);
+ 
+ private:
+  
+  //int len;
+  //int arr[];
+};
+
+#endif
